68b85c7eb36204a1ce7b5466bb32add46be5eb6c
Lennart Wisbar <lewisbar.git@gmail.com>
2017-07-01 22:16:01 +0200

    Added shooting and enemies

--- SpaceShooter/GameScene.swift
+++ SpaceShooter/GameScene.swift
@@ -15,6 +15,10 @@ class GameScene: SKScene {
     let backgroundEffect = SKEmitterNode(fileNamed: "BackgroundEffect")
     let spaceship = SKSpriteNode(imageNamed: "Spaceship")
     var musicPlayer: AVAudioPlayer?
+    var liveCount = 0
+    var liveArray = [SKSpriteNode]()
+    var timer1 = Timer()
+    var timer2 = Timer()
     
     override func didMove(to view: SKView) {
         
@@ -55,33 +59,51 @@ class GameScene: SKScene {
 
         // Music
         prepareMusic()
-//        if let musicURL = Bundle.main.url(forResource: "ExplosionSound", withExtension: "wav") {
-//            print("Audio file found")
-//            musicPlayer = try? AVAudioPlayer(contentsOf: musicURL)
-//            musicPlayer?.numberOfLoops = 0
-//            musicPlayer?.prepareToPlay()
-//        } else {
-//            print("Audio file not found")
-//        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if isPaused {
-            isPaused = false
-            let pauseBackground = childNode(withName: "pauseBackground")
-            let pauseLabel = childNode(withName: "pauseLabel")
-            
-            pauseBackground?.removeFromParent()
-            pauseLabel?.removeFromParent()
-            musicPlayer?.play()
-        }
         
+        // Lives
+        addLives(3)
+        
+        // Shoot
+        startSpaceshipFire()
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let touchPosition = touch.location(in: self)
-            spaceship.position.x = touchPosition.x
+    @objc func addEnemy() {
+        let enemy = SKSpriteNode(imageNamed: "Raumschiff")
+        let enemyStartX = CGFloat(arc4random_uniform(UInt32(self.size.width - 2 * enemy.size.width))) + enemy.size.width
+        let enemyStartY = self.size.height + enemy.size.height
+        enemy.position = CGPoint(x: enemyStartX, y: enemyStartY)
+        enemy.zPosition = 2
+        enemy.zRotation = .pi   // Upside down
+        self.addChild(enemy)
+        
+        // Action
+        let moveDown = SKAction.moveTo(y: -enemy.size.height, duration: 3)
+        let delete = SKAction.removeFromParent()
+        enemy.run(SKAction.sequence([moveDown, delete]))
+    }
+    
+    @objc func startSpaceshipFire() {
+        let fireBall = SKSpriteNode(imageNamed: "Sternschuss")
+        fireBall.position = spaceship.position
+        fireBall.zPosition = spaceship.zPosition - 1
+        self.addChild(fireBall)
+        
+        // Action
+        let shoot = SKAction.moveTo(y: self.size.height + fireBall.size.height, duration: 1)
+        let removeFireBall = SKAction.removeFromParent()
+        let fireSequence = SKAction.sequence([shoot, removeFireBall])
+        
+        fireBall.run(fireSequence)
+    }
+    
+    func addLives(_ liveCount: Int) {
+        for index in 1...liveCount {
+            let heart = SKSpriteNode(imageNamed: "Herz")
+            heart.position = CGPoint(x: CGFloat(index) * heart.size.width, y: self.size.height - heart.size.height)
+            heart.zPosition = 3
+            heart.name = "heart\(index)"
+            liveArray.append(heart)
+            self.addChild(heart)
         }
     }
     
@@ -98,5 +120,27 @@ class GameScene: SKScene {
         }
         musicPlayer?.numberOfLoops = 0
         musicPlayer?.prepareToPlay()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isPaused {
+            isPaused = false
+            let pauseBackground = childNode(withName: "pauseBackground")
+            let pauseLabel = childNode(withName: "pauseLabel")
+            
+            pauseBackground?.removeFromParent()
+            pauseLabel?.removeFromParent()
+            musicPlayer?.play()
+            
+            timer1 = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(startSpaceshipFire), userInfo: nil, repeats: true)
+            timer2 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(addEnemy), userInfo: nil, repeats: true)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchPosition = touch.location(in: self)
+            spaceship.position.x = touchPosition.x
+        }
     }
 }
